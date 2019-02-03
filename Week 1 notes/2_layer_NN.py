f1a11030a211d8bcf0296c6ee656bd2bd7baf9b1
mudit <muditchoraria97@gmail.com>
2019-02-04 02:51:43 +0530

    Added code for 2 layer neural network in python

---
+++ Week 1 notes/2_layer_NN.py
@@ -0,0 +1,187 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  4 00:19:53 2019
+
+@author: HP
+"""
+
+#This is a 2 layer neural network.
+
+#Package imports
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import *
+
+'''
+METHODOLOGY
+
+1. Define NN structure
+2. Initialize weights and biases
+3. Loop and implement forward propagation and backward propagation
+4. Update parameters
+'''
+
+def layer_sizes(X, Y):
+     """
+    Arguments:
+    X -- input dataset of shape (input size, number of examples)
+    Y -- labels of shape (output size, number of examples)
+    
+    Returns:
+    n_x -- the size of the input layer
+    n_h -- the size of the hidden layer
+    n_y -- the size of the output layer
+    """
+    
+    n_x = shape_X[0]
+    n_h = 4
+    n_y = shape_Y[0]
+    
+    return(n_x, n_h, n_y)
+
+define initialize_parameters(n_x, n_h, n_y):
+    """
+    Argument:
+    n_x -- size of the input layer
+    n_h -- size of the hidden layer
+    n_y -- size of the output layer
+    
+    Returns:
+    params -- python dictionary containing your parameters:
+        W1 -- weight matrix of shape (n_h, n_x)
+        b1 -- bias vector of shape (n_h, 1)
+        W2 -- weight matrix of shape (n_y, n_h)
+        b2 -- bias vector of shape (n_y, 1)
+    """
+    
+        #To simplify the equation when calculating the activation
+        #so as to not have a transpose in the calculation we have swapped
+        #first and second value
+        
+        W1 = np.random.randn(n_h, n_x) * 0.01
+        b1 = np.zeros(shape=(n_h, 1))
+        W2 = np.random.randn(n_y, n_h) * 0.01
+        b2 = np.zeros(shape=(n_y, 1))
+        
+        parameters = {"W1": W1,
+                      "b1": b1,
+                      "W2": W2,
+                      "b2": b2}
+        
+        return parameters
+        
+define sigmoid(x, derivative = False):
+    return x * (1-x) if derivative else 1 / (1 + np.exp(-x))
+
+define forward_propagation(X, parameters):
+     """
+    Arguments:
+    X -- input data of size (n_x, m)
+    parameters -- python dictionary containing your parameters (output of initialization function)
+    
+    Returns:
+    A2 -- The sigmoid output of the second activation
+    cache -- a dictionary containing "Z1", "A1", "Z2" and "A2"
+    """
+    W1 = parameters["W1"]
+    b1 = parameters["b1"]
+    W2 = parameters["W2"]
+    b2 = parameters["b2"]
+    
+    Z1 = np.dot(W1, X) + b1
+    A1 = np.tanh(Z1)
+    Z2 = np.dot(W2, A1) + b2
+    A2 = sigmoid(Z2)
+    
+    cache = {"Z1": Z1,
+             "A1": A1,
+             "Z2": Z2,
+             "A2": A2}
+    
+    return A2, cache
+
+define compute_cost(A2, Y, parameters):
+    """
+    Computes the cross-entropy cost     
+    Arguments:
+    A2 -- The sigmoid output of the second activation, of shape (1, number of examples)
+    Y -- "true" labels vector of shape (1, number of examples)
+    parameters -- python dictionary containing your parameters W1, b1, W2 and b2
+    
+    Returns:
+    cost -- cross-entropy cost 
+    """
+    
+    m = Y.shape[1]
+    logprobs = np.multiply(np.log(A2), Y) + np.multiply(np.log(1 - A2), 1 - Y)
+    cost -= np.sum(logprobs)/m
+    
+    cost = np.squeeze(cost) # makes sure cost is the dimension we expect. 
+                            # E.g., turns [[17]] into 17 
+    return cost    
+
+define backward_propagation(parameters, cache, X, Y):
+    """
+    Implement the backward propagation using the instructions above.
+    
+    Arguments:
+    parameters -- python dictionary containing our parameters 
+    cache -- a dictionary containing "Z1", "A1", "Z2" and "A2".
+    X -- input data of shape (2, number of examples)
+    Y -- "true" labels vector of shape (1, number of examples)
+    
+    Returns:
+    grads -- python dictionary containing your gradients with respect to different parameters
+    """
+    m = X.shape[1]
+    
+    W1 = parameters["W1"]
+    W2 = parameters["W2"]
+    A1 = cache["A1"]
+    A2 = cache["A2"]
+    
+    dZ2 = A2 - Y
+    dW2 = (1 / m) * np.dot(dZ2, A1.T)
+    db2 = (1 / m) * np.sum(dZ2, axis, keepdims = True)
+    dZ1 = np.multiply(np.dot(W2.T, dZ2), 1 - np.power(A1, 2))
+    dW1 = (1 / m) * np.dot(dZ1, X.T)
+    db1 = (1 / m) * np.sum(dZ1, axis = 1, keepdims =True)
+    
+    grads = {"dW1": dW1,
+             "db1": db1,
+             "dW2": dW2,
+             "db2": db2}
+    
+    return grads
+
+def update_parameters(parameters, grads, learning_rate = 1.2):
+    """
+    Updates parameters using the gradient descent update rule given above
+    
+    Arguments:
+    parameters -- python dictionary containing your parameters 
+    grads -- python dictionary containing your gradients 
+    
+    Returns:
+    parameters -- python dictionary containing your updated parameters 
+    """
+    W1 = parameters['W1']
+    b1 = parameters['b1']
+    W2 = parameters['W2']
+    b2 = parameters['b2']
+    dW1 = grads['dW1']
+    db1 = grads['db1']
+    dW2 = grads['dW2']
+    db2 = grads['db2']
+    
+    W1 = W1 - learning_rate * dW1
+    b1 = b1 - learning_rate * db1
+    W2 = W2 - learning_rate * dW2
+    b2 = b2 - learning_rate * db2
+    
+    parameters = {"W1": W1,
+                  "b1": b1,
+                  "W2": W2,
+                  "b2": b2}
+    
+    return parameters
